7af0442ecbaa8a82ac09c85dc4afed78856efe88
akmalxon1 <akmalxon359@gmail.com>
2024-08-15 13:52:25 +0500

    Biror bir xabar

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let names = prompt('Iltimos, ismingizni kiriting:');
+let spaceId = prompt('Iltimos, space ID ni kiriting:');
+let spacePassword = prompt('Iltimos, space parolini kiriting:');
+
+
+let userInfo = {
+  names: names,
+  id: spaceId,
+  password: spacePassword
+};
+
+
+console.log(userInfo);
